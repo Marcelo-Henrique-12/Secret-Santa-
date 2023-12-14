d6c10e3f9eb8a793d5f7a5ee7505b03e1e6b6caa
Marcelo Henrique de Freitas <marcelo.freitas@campus.unimontes.int>
2023-12-14 11:05:18 -0300

    custon adminlte

--- resources/views/sorteio/index.blade.php
+++ resources/views/sorteio/index.blade.php
@@ -25,69 +25,74 @@
     <div class="lista">
         <h3>Sorteios</h3>
         <p>Sorteios realizados</p>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Nome do Sorteio</th>
-                    <th scope="col">Quantidade de sorteios realizados</th>
-                    <th scope="col">Ações</th>
-                    <th scope="col"></th> {{-- Nova coluna para o botão de e-mail --}}
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($sorteados as $sorteado)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $sorteado->sorteio_id }}</td>
-                        <td>{{ $sorteado->sorteio->nome }}</td>
-                        <td>{{ $sorteado->quantidade }}</td>
-                        <td>
-                            <div class="btn-group" role="group" aria-label="Ações">
-                                <form method="POST" action="{{ route('sorteio.email', $sorteado->sorteio_id) }}">
-                                    @csrf
-                                    <button type="submit" class="btn btn-info" title="Enviar E-mail">
-                                        <i class="fas fa-envelope"></i>
-                                    </button>
-                                </form>
-                                <a href="#" type="button" class="btn btn-danger" data-toggle="modal"
-                                    data-target="#modal-default{{ $sorteado->sorteio_id }}" title="Excluir"
-                                    style="margin-left: 5px">
-                                    <i class="fas fa-trash-alt"></i>
-                                </a>
-                            </div>
-                        </td>
-
-                        {{-- DELETE MODAL --}}
-                        <div class="modal fade" id="modal-default{{ $sorteado->sorteio_id }}" style="display: none;"
-                            aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4 class="modal-title">Excluir o sorteio deste ano!</h4>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">×</span>
+                        <th scope="col">Id</th>
+                        <th scope="col">Nome do Sorteio</th>
+                        <th scope="col">Quantidade de sorteios realizados</th>
+                        <th scope="col">Ações</th>
+                        <th scope="col"></th> {{-- Nova coluna para o botão de e-mail --}}
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($sorteados as $sorteado)
+                        <tr>
+                            <td>{{ $sorteado->sorteio_id }}</td>
+                            <td>{{ $sorteado->sorteio->nome }}</td>
+                            <td>{{ $sorteado->quantidade }}</td>
+                            <td>
+                                <div class="btn-group" role="group" aria-label="Ações">
+                                    <form method="POST" action="{{ route('sorteio.email', $sorteado->sorteio_id) }}">
+                                        @csrf
+                                        <button type="submit" class="btn btn-info" title="Enviar E-mail">
+                                            <i class="fas fa-envelope"></i>
                                         </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>Todos os sorteios deste ano serão cancelados. Deseja realmente excluir?</p>
-                                    </div>
-                                    <div class="modal-footer justify-content-between">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-                                        <form method="post" action="{{ route('sorteio.destroy', $sorteado->sorteio_id) }}">
-                                            @method('delete')
-                                            @csrf
-                                            <input type="hidden" name="rota" value="{{ Route::currentRouteName() }}">
-                                            <button type="submit" class="btn btn-danger">Excluir</button>
-                                        </form>
+                                    </form>
+                                    <a href="#" type="button" class="btn btn-danger" data-toggle="modal"
+                                        data-target="#modal-default{{ $sorteado->sorteio_id }}" title="Excluir"
+                                        style="margin-left: 5px">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                </div>
+                            </td>
+
+                            {{-- DELETE MODAL --}}
+                            <div class="modal fade" id="modal-default{{ $sorteado->sorteio_id }}" style="display: none;"
+                                aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">Excluir o sorteio deste ano!</h4>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Todos os sorteios deste ano serão cancelados. Deseja realmente excluir?</p>
+                                        </div>
+                                        <div class="modal-footer justify-content-between">
+                                            <button type="button" class="btn btn-default"
+                                                data-dismiss="modal">Fechar</button>
+                                            <form method="post"
+                                                action="{{ route('sorteio.destroy', $sorteado->sorteio_id) }}">
+                                                @method('delete')
+                                                @csrf
+                                                <input type="hidden" name="rota"
+                                                    value="{{ Route::currentRouteName() }}">
+                                                <button type="submit" class="btn btn-danger">Excluir</button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        {{-- DELETE MODAL --}}
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                            {{-- DELETE MODAL --}}
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
 @stop
@@ -96,7 +101,7 @@
     <link rel="stylesheet" href="/css/admin_custom.css">
     <style>
         .lista {
-            margin-top: 100px;
+            margin-top: 2em;
         }
     </style>
 @stop
